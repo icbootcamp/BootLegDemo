11069783b5fb692cafd92815552b0cb8a9c91182
craigrabbitt <craigrabbitt@VIRTUALWIN10>
2017-03-27 15:31:14 +1300

    Tidied Code

--- BootLegDemo/Controllers/HomeController.cs
+++ BootLegDemo/Controllers/HomeController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BootLegDemo.Models;
 
 namespace BootLegDemo.Controllers
 {
     public class HomeController : Controller
     {
+        BootLegEntities db = new BootLegEntities();
+
         public ActionResult Index()
         {
             return View();
@@ -25,6 +28,38 @@ namespace BootLegDemo.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult GetModelList()
+        {          
+            var list = db.People.ToList().Select(x => new SupplierListModel
+            {
+                Id = x.Id,
+                FirstName = x.FirstName,
+                CompanyName = x.Supplier.CompanyName
+            }).ToList();
+            return View(list);
+        }
+
+        public ActionResult EditView(int id)
+        {
+            var currentPerson = db.People.Find(id);
+            var sType = db.SupplierTypes.ToList();
+            SupplierEntryModel model = new SupplierEntryModel
+            { Id = currentPerson.Id,
+                CompanyName = currentPerson.Supplier.CompanyName,
+                FirstName = currentPerson.FirstName,
+                SupplierType = sType.
+                Select(x => new SelectListItem { Text = x.Name, Value = x.Id.ToString() }).
+                ToList()
+            };
+
+            return View(model);
+        }
+
+        public ActionResult SaveEdit(SupplierEntryModel model)
+        {
+            return Json(new { success = true });
         }
     }
 }
---
+++ BootLegDemo/Models/SuppplierListModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BootLegDemo.Models
+{
+    public class SupplierListModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string CompanyName { get; set; }
+    }
+
+    public class SupplierEntryModel : SupplierListModel
+    {
+        public List<SelectListItem> SupplierType { get; set; }
+        public int SupplierTypeId { get; set; }
+    }
+}
